5a9333a8e1a5cc9e378473b8170a235e9cefac13
Sharukhan ⚙️ DevOps Maestro <106614101+sharukhannadagoud@users.noreply.github.com>
2026-09-23 08:15:59 +0000

    fixed sh file

--- .github/workflows/ascii.sh
+++ .github/workflows/ascii.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
 echo "My first GitHub Actions job"
-ls -ltra       
+
+ls -ltra
+
 cat README.md
+
 sudo apt-get update && sudo apt-get install -y cowsay
+
 cowsay -f dragon "Run for cover, I am a dragon...RAWR" >> dragon.txt
