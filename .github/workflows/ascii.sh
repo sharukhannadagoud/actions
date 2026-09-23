cd0798066f13a01fde207c33ff52f4f5480e076d
Sharukhan ⚙️ DevOps Maestro <106614101+sharukhannadagoud@users.noreply.github.com>
2026-09-23 08:13:32 +0000

    added dragon.txt

--- .github/workflows/ascii.sh
+++ .github/workflows/ascii.sh
@@ -2,4 +2,4 @@ echo "My first GitHub Actions job"
 ls -ltra       
 cat README.md
 sudo apt-get update && sudo apt-get install -y cowsay
-cowsay -f dragon "Run for cover, I am a dragon...RAWR"
+cowsay -f dragon "Run for cover, I am a dragon...RAWR" >> dragon.txt
